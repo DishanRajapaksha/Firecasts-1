e8d37f28e28695429114da537f6575b6cef14e77
Savithra <it14118982@my.sliit.lk>
2016-04-03 18:50:14 +0530

    Location Services added

--- app/src/main/java/com/firebasetest/MainActivity.java
+++ app/src/main/java/com/firebasetest/MainActivity.java
@@ -1,8 +1,19 @@
 package com.firebasetest;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.media.audiofx.BassBoost;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -15,18 +26,34 @@ import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
 import com.firebase.client.ValueEventListener;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     TextView mTextFieldCondition;
+    TextView mTextFieldLocation;
+    TextView mTextFieldAddress;
     Button mButtonSunny;
     Button mButtonFoggy;
     Firebase mRef;
+    private LocationListener locationListener;
+    private LocationManager locationManager;
+    private RequestQueue requestQueue;
+    double Lat,Long;
+    String key = "AIzaSyDPjgyikV5AarP4PWmcIbCqoihefz7LAkY";
+    String Address;
 
 
     @Override
@@ -56,39 +83,161 @@ public class MainActivity extends AppCompatActivity
     }
 
     @Override
-    protected  void onStart(){
+    protected void onStart() {
         super.onStart();
         mTextFieldCondition = (TextView) findViewById(R.id.textViewCondition);
+        mTextFieldLocation = (TextView) findViewById(R.id.textViewLocation);
+        mTextFieldAddress = (TextView) findViewById(R.id.textViewAddress);
         mButtonFoggy = (Button) findViewById(R.id.buttonFoggy);
         mButtonSunny = (Button) findViewById(R.id.buttonSunny);
+
+        //Location Services Start
+
+        requestQueue = Volley.newRequestQueue(this);
+
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                Lat = location.getLatitude();
+                Long = location.getLongitude();
+                mRef.child("location").setValue(Lat+" "+Long);
+                getAddress();
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+                Intent locationSettings = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(locationSettings);
+            }
+        };
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{
+                        Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.INTERNET
+                },10);
+                return;
+            }
+            else{
+                locationManager.requestLocationUpdates("gps", 50000, 50, locationListener);
+            }
+        }
+        locationManager.requestLocationUpdates("gps", 50000, 50, locationListener);
+
+        //End Location Services
+
+        //Firebase Services
         mRef = new Firebase("https://hellooworldd.firebaseio.com/conditionAndroid");
-        mRef.addValueEventListener(new ValueEventListener() {
+
+        mRef.child("condition").addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                String text = dataSnapshot.getValue(String.class);
-                mTextFieldCondition.setText(text);
+                String condition = dataSnapshot.getValue(String.class);
+                mTextFieldCondition.setText(condition);
             }
 
             @Override
             public void onCancelled(FirebaseError firebaseError) {
+            }
+        });
 
+        mRef.child("location").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String coordinates = dataSnapshot.getValue(String.class);
+                mTextFieldLocation.setText(coordinates);
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+            }
+        });
+
+        mRef.child("address").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String add = dataSnapshot.getValue(String.class);
+                mTextFieldAddress.setText(add);
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
             }
         });
 
         mButtonSunny.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mRef.setValue("Sunny");
+                mRef.child("condition").setValue("Sunny");
             }
         });
 
         mButtonFoggy.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mRef.setValue("Foggy");
+                mRef.child("condition").setValue("Foggy");
             }
         });
+
+        //Firebase Services End
+
     }
+
+    //Location Services Methods Start
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case 10:
+                if (grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                    //configureButton();
+                    locationManager.requestLocationUpdates("gps", 50000, 50, locationListener);
+                return;
+        }
+    }
+
+//    private void configureButton() {
+//        mButtonGetLocation.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View view){
+//
+//            }
+//        });
+//    }
+
+    public void getAddress(){
+        JsonObjectRequest request = new JsonObjectRequest("https://maps.googleapis.com/maps/api/geocode/json?latlng=" + Lat + "," + Long + "&Key=" + key, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    Address = response.getJSONArray("results").getJSONObject(0).getString("formatted_address");
+                    mRef.child("address").setValue(Address);
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+
+        requestQueue.add(request);
+    }
+
+    //Location Services Methods End
 
     @Override
     public void onBackPressed() {
